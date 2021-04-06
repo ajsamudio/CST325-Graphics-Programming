5bc1d05b189b2473d0bf375745554d2466fd66db
ajsamudio <70294918+ajsamudio@users.noreply.github.com>
2021-04-05 18:39:35 -0700

    Add files via upload

---
+++ WebGL2/misc/camera.js
@@ -0,0 +1,61 @@
+function Camera(input) {
+    // The following two parameters will be used to automatically create the cameraWorldMatrix in this.update()
+    this.cameraYaw = 0;
+    this.cameraPosition = new Vector3();
+
+    this.cameraWorldMatrix = new Matrix4();
+
+    // -------------------------------------------------------------------------
+    this.getViewMatrix = function() {
+        return this.cameraWorldMatrix.clone().inverse();
+    }
+
+    // -------------------------------------------------------------------------
+    this.getForward = function() {
+        // todo #6 - pull out the forward direction from the world matrix and return as a vector
+        //         - recall that the camera looks in the "backwards" direction
+
+
+        var xAxis = this.cameraWorldMatrix.clone().getElement(0,3);
+        var yAxis = this.cameraWorldMatrix.clone().getElement(1,3);
+        var zAxis = this.cameraWorldMatrix.clone().getElement(2,3);
+
+        return new Vector3(xAxis,yAxis,zAxis);
+    }
+    // -------------------------------------------------------------------------
+    this.update = function(dt) {
+        var currentForward = this.getForward();
+
+        if (input.up) {
+            // todo #7 - move the camera position a little bit in its forward direction
+             this.cameraPosition.subtract(currentForward);
+        }
+
+        if (input.down) {
+            // todo #7 - move the camera position a little bit in its backward direction
+            this.cameraPosition.add(currentForward);
+
+        }
+
+        if (input.left) {
+            // todo #8 - add a little bit to the current camera yaw
+            this.cameraYaw+=0.5;
+
+        }
+
+        if (input.right) {
+            // todo #8 - subtract a little bit from the current camera yaw
+            this.cameraYaw-=0.5;
+
+        }
+
+        // todo #7 - create the cameraWorldMatrix from scratch based on this.cameraPosition
+        this.cameraWorldMatrix.makeIdentity();
+                this.cameraWorldMatrix.makeTranslation(this.cameraPosition);
+        // todo #8 - create a rotation matrix based on cameraYaw and apply it to the cameraWorldMatrix
+        // (order matters!)
+        var rotate=new Matrix4().makeRotationY(this.cameraYaw);
+        this.cameraWorldMatrix.multiply(rotate);
+
+    }
+}
