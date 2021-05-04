751c90c45dacecb1da4dc81e4755612af330895c
ajsamudio <ajayyys3@gmail.com>
2021-05-03 23:40:40 -0700

    shadowmap4.0

--- Shadow Mapping/shaders/depth-write.vs.glsl
+++ Shadow Mapping/shaders/depth-write.vs.glsl
@@ -1,15 +1,16 @@
+attribute vec3 aVertexPosition;
 
-    attribute vec3 aVertexPosition;
+   uniform mat4 uWorldMatrix;
+   uniform mat4 uViewMatrix;
+   uniform mat4 uProjectionMatrix;
 
-    uniform mat4 uWorldMatrix;
-    uniform mat4 uViewMatrix;
-    uniform mat4 uProjectionMatrix;
+   varying float vDepth;
 
-    varying float vDepth;
+   void main(void) {
+       gl_Position = uProjectionMatrix * uViewMatrix * uWorldMatrix * vec4(aVertexPosition, 1.0);
 
-    void main(void) {
-        gl_Position = uProjectionMatrix * uViewMatrix * uWorldMatrix * vec4(aVertexPosition, 1.0);
+       // todo #3 convert clip space depth into NDC and rescale from [-1, 1] to [0, 1]
+       gl_Position.z=(gl_Position.z/2.0)+ 0.5;
+       vDepth = gl_Position.z;  // temporarily set to gl_Position.z
 
-        // todo #3 convert clip space depth into NDC and rescale from [-1, 1] to [0, 1]
-        vDepth = gl_Position.z;  // temporarily set to gl_Position.z
-    }
+   }
